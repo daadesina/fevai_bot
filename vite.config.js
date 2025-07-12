b1f70607a35c3cd377b3d87cc9beefdaa58a119e
daadesina <daadesina1@gmail.com>
2025-07-12 16:51:45 +0100

    vite config edited for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT) || 5173, // use Render's PORT if available
+    port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['fevai-bot.onrender.com'], // 👈 Add this line
   },
 })
