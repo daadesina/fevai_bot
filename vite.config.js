19250f6ec68cfabb7ba1f9cfb1136591bb23a82d
daadesina <daadesina1@gmail.com>
2025-07-12 16:24:19 +0100

    vite config edited for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173, // use Render's PORT if available
+  },
 })
